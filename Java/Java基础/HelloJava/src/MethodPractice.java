5501d017f2c07515f7b4f964e45c6dea5afefbe7
MaoYu <2874553847@qq.com>
2025-07-16 14:14:27 +0800

    第一阶段基础代码练手

---
+++ Java/Java基础/HelloJava/src/MethodPractice.java
@@ -0,0 +1,14 @@
+public class MethodPractice {
+    public static void main(String[] args) {
+        System.out.println("程序开始！");
+        printSeparator();
+        System.out.println("Self introduction");
+        System.out.println("I am learning Java!");
+        printSeparator();
+        System.out.println("程序结束！");
+    }
+
+    public static void printSeparator() {
+        System.out.println("====================");
+    }
+}
