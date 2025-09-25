489cd8e71d3cf16dfa36f35b71744ec060aa79db
maoyu <2874553847@qq.com>
2025-09-25 18:25:48 +0800

    新增my-blog项目

---
+++ my-blog/src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-slate-800 h-screen flex justify-center items-center">
+    <h1 class="text-3xl font-bold text-white underline">Hello TailwindCSS!</h1>
+  </div>
+</template>
