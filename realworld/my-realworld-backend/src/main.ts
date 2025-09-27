90659688494f27095df6738d255d002e7c8be49e
maoyu <2874553847@qq.com>
2025-09-27 18:01:45 +0800

    新增做题

--- realworld/my-realworld-backend/src/main.ts
+++ realworld/my-realworld-backend/src/main.ts
@@ -4,8 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
   app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((err) => {
   // 如果 bootstrap 过程中出现任何错误，这里会捕获到
---
+++ 前端/3.  JavaScript 学习/1. JS 基础/null-undefined 注意事项.md
@@ -0,0 +1,10 @@
+声明但未赋值----->undefined
+
+赋值了，但是值为空------>null
+
+```js
+console.log(undefined + 1);  // NaN
+console.log(null + 1); // 1
+console.log(undefined + null);	// NaN
+```
+
