d17d9d74451a5a45a4d957dacf496ee3824480df
maoyu <2874553847@qq.com>
2025-10-17 19:14:24 +0800

    1

--- my-blog/src/App.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="bg-slate-800 h-screen flex justify-center items-center">
-    <h1 class="text-3xl font-bold text-white underline">Hello TailwindCSS!</h1>
-  </div>
-</template>
