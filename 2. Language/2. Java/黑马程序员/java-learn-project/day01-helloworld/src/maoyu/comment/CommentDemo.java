4da194159d66a81c835c20e7b6d51c57b7987354
MaoYu <2874553847@qq.com>
2025-11-12 21:18:39 +0800

    1

---
+++ 2. Language/2. Java/黑马程序员/java-learn-project/day01-helloworld/src/maoyu/comment/CommentDemo.java
@@ -0,0 +1,24 @@
+package maoyu.comment;
+/**
+ * 1. 单行注释
+ * 2. 多行注释
+ * 3. 文档注释
+ */
+public class CommentDemo {
+    /**
+     * 这是一个方法
+     * 主方法
+     */
+    public static void main(String[] args) {
+        // 1. 单行注释
+        // 打印两行诗句
+        // 打印古诗《悯农》的第一句
+        System.out.println("锄禾日当午，");
+        // 打印古诗《悯农》的第二句
+        System.out.println("汗滴禾下土。");
+
+        /*
+            2. 多行注释
+        */
+    }
+}
---
+++ 2. Language/2. Java/黑马程序员/java-learn-project/day01-helloworld/src/maoyu/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package maoyu.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+        // 打印10行hello world
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World !");
+        }
+    }
+}
