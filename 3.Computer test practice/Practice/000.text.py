5f654f99a46b1edf4b3ad1268e410d872fff5a83
MaoYu <2874553847@qq.com>
2025-12-16 23:58:23 +0800

    1

--- 3.Computer test practice/Practice/000.text.py
+++ 3.Computer test practice/Practice/000.text.py
@@ -1,70 +1,24 @@
 import sys
 
-
 def solve():
-    line1 = sys.stdin.readline().strip()
-    if not line1:
-        return []
-    cores = list(map(int, line1.split()))
+    try:
+        line = sys.stdin.readline().strip()
+        if not line:
+            return
+        array = list(map(int, line.split()))
+    except ValueError:
+        print([])
+        return
 
-    cores.sort()
-
-    line2 = sys.stdin.readline().strip()
-    num = int(line2)
-
-    link1 = []
-    link2 = []
-
-    for core in cores:
-        if core < 4:
-            link1.append(core)
+    n = len(array)
+    for i in range(0, n-1):
+        if i % 2 == 0:
+            if array[i] < array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
         else:
-            link2.append(core)
-    if num == 1:
-        if len(link1) == 1:
-            return link1
-        elif len(link2) == 1:
-            return link2
-        elif len(link1) == 3:
-            return link1
-        elif len(link2) == 3:
-            return link2
-        elif len(link1) == 2:
-            return link1
-        elif len(link2) == 2:
-            return link2
-        elif len(link1) == 4:
-            return link1
-        elif len(link2) == 4:
-            return link2
-        else:
-            return []
-    if num == 2:
-        if len(link1) == 2:
-            return link1
-        elif len(link2) == 2:
-            return link2
-        elif len(link1) == 4:
-            return link1
-        elif len(link2) == 4:
-            return link2
-        elif len(link1) == 3:
-            return link1
-        elif len(link2) == 3:
-            return link2
-        else:
-            return []
-    if num == 4:
-        if len(link1) == 4:
-            return link1
-        elif len(link2) == 4:
-            return link2
-        else:
-            return []
-    if num == 8:
-        if len(cores) == 0:
-            return cores
+            if array[i] > array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
+    print(" ".join(str(num) for num in array))
 
-
-if __name__ == "__main__":
-    print(solve())
+if __name__ == '__main__':
+    solve()
