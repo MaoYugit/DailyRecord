127527319834e7345121489bd75857dfcb85cb9c
MaoYu <2874553847@qq.com>
2025-12-18 07:59:17 +0800

    1

--- 3.Computer test practice/Practice/000.text.py
+++ 3.Computer test practice/Practice/000.text.py
@@ -1,24 +1,52 @@
 import sys
 
+
 def solve():
     try:
-        line = sys.stdin.readline().strip()
-        if not line:
+        line1 = sys.stdin.readline().strip()
+        if not line1:
             return
-        array = list(map(int, line.split()))
+        n = int(line1)
+        target_document = sys.stdin.readline().strip()
     except ValueError:
-        print([])
         return
 
-    n = len(array)
-    for i in range(0, n-1):
-        if i % 2 == 0:
-            if array[i] < array[i+1]:
-                array[i], array[i+1] = array[i+1], array[i]
-        else:
-            if array[i] > array[i+1]:
-                array[i], array[i+1] = array[i+1], array[i]
-    print(" ".join(str(num) for num in array))
+    file_list = []
+    for i in range(n):
+        count = 0
+        line = sys.stdin.readline().strip()
+        for char in line:
+            if char == "-":
+                count += 1
+                continue
+            else:
+                break
+        folder = line[count:]
+        if folder == target_document+ " -1":
+            for j in range(i+1, n):
+                file_line = sys.stdin.readline().strip()
+                s_s = "-" * (count + 4)
+                if not file_line.startswith(s_s):
+                    break
+                cur_list = file_line.split()
+                if cur_list[1] == "-1":
+                    continue
+                else:
+                    cur_file_name = ""
+                    for char in cur_list[0]:
+                        if char == "-":
+                            continue
+                        else:
+                            cur_file_name += char
+                    file_list.append((cur_file_name, cur_list[1]))
+                break
+
+    if len(file_list) == 0:
+        print("No file")
+    file_list.sort(key=lambda x:x[1])
+
+    for file_name, create_time in file_list:
+        print(file_name, create_time)
 
 if __name__ == '__main__':
     solve()
